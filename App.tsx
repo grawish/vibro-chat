c12eedc6d3e0c50b00b2ae90f5d169ea03ba946d
grawish <v-gsachdeva@swiftly.com>
2023-01-14 12:38:56 +0530

    setup Nativewind: tailwind for react native

--- App.tsx
+++ App.tsx
@@ -4,7 +4,7 @@ import { StyleSheet, Text, View } from "react-native";
 
 export default function App() {
   return (
-    <View style={styles.container}>
+    <View style={styles.container} className={"bg-amber-200"}>
       <Text>Open up App.tsx to start working on your app!</Text>
       <StatusBar style="auto" />
     </View>
@@ -14,7 +14,6 @@ export default function App() {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: "#fff",
     alignItems: "center",
     justifyContent: "center",
   },
